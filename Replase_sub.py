d496ef330a4a113ea2520136db7014c28288ff43
Llona <liu.lon@gmail.com>
2016-08-05 16:34:45 +0800

    Rewrite for more module implement

--- Replase_sub.py
+++ Replase_sub.py
@@ -5,55 +5,86 @@ Ver 2.0 - Uses database for match SUB file name and read Sublist.ini for match S
 Ver 3.0 - Uses GUI for parameter input
 Ver 4.0 - Re-develop this application by Python3
 """
-import re
-import os
 import glob
+import os
+import re
 
-subdata_tmp = "" #Temp var for read file line
-subdata_dic = {} #sub database
-subpath = 'D:\ScripFile\python\Replase_sub\\test_sub' #ssa or ass sub file path
-subfile_list = [] #All sub file list that wait to replase
-subtemp_list = [] #temp var for store sub list
-sub_content = "" #var for store sub file's content
-subfiletype_list = ['*.ssa', '*.ass'] #sub file type, ex: *.ssa ,*.ass...
+subdata_dic = {}  # sub database
+subpath = 'D:\ScripFile\python\Replase_sub\\test_sub'  # ssa or ass sub file path
+subfile_list = []  # All sub file list that wait to replace
+subfiletype_list = ['*.ssa', '*.ass']  # sub file type, ex: *.ssa ,*.ass...
 
-#-----Read sub databse file SubList.sdb and store to dict structure-----
-sublist_h = open("SubList.sdb", 'r', encoding='utf16')
-while True:
-    subdata_tmp = sublist_h.readline()
-    if subdata_tmp:
-        re_h = re.match(r'(.+);(.+)', subdata_tmp)
-        subdata_dic[re_h.group(1)] = re_h.group(2)
-        #print(re_h.group(1), re_h.group(2))
-    else:
-        break
+"""
+# -----Read sub databse file SubList.sdb and store to dict structure-----
+Return: string database store in dic structure
+"""
+def get_database_list():
+    subdata_dic_ld = {}
+    subdata_tmp_lv = ''
 
-sublist_h.close()
+    sublist_h = open("SubList.sdb", 'r', encoding='utf16')
+    while True:
+        subdata_tmp_lv = sublist_h.readline()
+        if subdata_tmp_lv:
+            re_h = re.match(r'(.+);(.+)', subdata_tmp_lv)
+            subdata_dic_ld[re_h.group(1)] = re_h.group(2)
+            # print(re_h.group(1), re_h.group(2))
+        else:
+            break
+    sublist_h.close()
+    return subdata_dic_ld
 
-#-----Get all sub file list that need to replace-----
-os.chdir(subpath)
-for i in subfiletype_list:
-    subtemp_list=glob.glob(i)
-    if subtemp_list:
-        subfile_list.extend(subtemp_list)
+"""
+# -----Get all sub file list that need to replace-----
+1. sub_path: sub file path
+2. sub file type (example: *.ssa, *.ass)
+Return: all sub file list
+"""
+def get_file_list(sub_path, file_type):
+    subfile_list_ll = []
+    subtemp_list_ll = []  # temp var for store sub list
 
-#-----Read all sub file and replace string that define in database file-----
-for i in subfile_list:
-    subcontent_read_h = open(i, 'r+', encoding='utf16')
-    sub_content = subcontent_read_h.read()
-    sub_content_temp = sub_content
+    os.chdir(sub_path)
+    for i in file_type:
+        subtemp_list_ll = glob.glob(i)
+        if subtemp_list_ll:
+            subfile_list_ll.extend(subtemp_list_ll)
+    return subfile_list_ll
 
-    for j, v in subdata_dic.items():
-        #print(j,v)
-        sub_content = sub_content.replace(j,v)
-    if sub_content_temp != sub_content:
-        subcontent_read_h.seek(0,0)
-        subcontent_read_h.write(sub_content)
-        #print("Find string need to modify")
-    else:
-        print("Sub file doesn't need to change:", i)
-        
-    #print(sub_content)
-    sub_content = ''  #clean temp var
-    sub_content_temp = '' #clean temp var
-    subcontent_read_h.close()
+"""
+# -----Read all sub file and replace string that define in database file-----
+1. sub file list
+2. string database store in dic structure
+Return: none
+"""
+def replace_string_write_to_file(subfile_list, subdata_dic):
+    for i in subfile_list:
+        sub_content_lv = ''
+        sub_content_temp_lv = ''
+
+        subcontent_read_h = open(i, 'r+', encoding='utf16')
+        sub_content_lv = subcontent_read_h.read()
+        sub_content_temp_lv = sub_content_lv
+
+        for j, v in subdata_dic.items():
+            #print(j, v)
+            sub_content_lv = sub_content_lv.replace(j, v)
+        if sub_content_temp_lv != sub_content_lv:
+            subcontent_read_h.seek(0, 0)
+            subcontent_read_h.write(sub_content_lv)
+            # print("Find string need to modify")
+        else:
+            pass
+            print("Sub file doesn't need to change:", i)
+        # print(sub_content)
+        subcontent_read_h.close()
+
+
+# Get database list to dic structure
+subdata_dic = get_database_list()
+# Get sub file list by type
+subfile_list = get_file_list(subpath, subfiletype_list)
+# Replace all file list string by dic structure
+replace_string_write_to_file(subfile_list, subdata_dic)
+
+print("Replace Done")
