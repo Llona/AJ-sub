91b0e3364f57e6a61a553ba6a9b192def4d388e7
Llona <liu.lon@gmail.com>
2016-08-23 14:23:36 +0800

    Change main process for error handling Add error handling for file R/W error handling

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ Ver 4.3.0 -
     1. Add log widge and print log into log widge
     2. Add convert clipboard function
     3. Modify main process for error handling
+    4. Add R/W setting file error handling
 """
 
 from tkinter import *
@@ -22,6 +23,7 @@ import configparser
 import os
 import shutil
 from tkinter.scrolledtext import ScrolledText
+from enum import Enum
 
 import replace_sub
 import langconver
@@ -54,6 +56,12 @@ help_text = \
     "[Yichen (Eugene)](https://github.com/yichen0831/opencc-python).\n"\
 
 
+
+class error_Type(Enum):
+    NORMAL = 'NORMAL'  # define normal state
+    FILE_ERROR = 'FILE_RW_ERROR'  # define file o/r/w error type
+
+
 class replace_Sub_Gui(Frame):
     def __init__(self, master=None, subfilepath_ini=None, subfiletype_ini=None, help_text=None):
         Frame.__init__(self, master)
@@ -82,6 +90,7 @@ class replace_Sub_Gui(Frame):
         # -----Set Text log fone color-----
         self.log.tag_config("error", foreground="#CC0000")
         self.log.tag_config("info", foreground="#008800")
+        self.log.tag_config("info2", foreground="#404040")
 
         root.bind('<Key-Return>', self.press_key_enter)
 
@@ -126,8 +135,8 @@ class replace_Sub_Gui(Frame):
         self.version_state.grid(row=4, column=0, columnspan=2, sticky='w')
         # self.version_state.grid(row=3, column=0, sticky='w')
         # -----Text log-----
-        self.log["width"] = 80
-        # self.log["font"] = ("Purisa", 10)
+        self.log["width"] = 60
+        self.log["font"] = ("Purisa", 10)
         self.log.grid(row=5, column=0, columnspan=10, sticky='NSWE')
 
     def press_key_enter(self, event):
@@ -163,7 +172,7 @@ class replace_Sub_Gui(Frame):
     def setlog(self, string, level=None):
         self.log.config(state="normal")
 
-        if (level != 'error') and (level != 'info'):
+        if (level != 'error') and (level != 'info') and (level != 'info2'):
             level = ""
 
         self.log.insert(INSERT, string + "\n", level)
@@ -181,8 +190,9 @@ class replace_Sub_Gui(Frame):
             file_ini_lh.close()
             return config_lh.get(section, key)
         except:
-            self.setlog("Error! Read setting to ini file fail, "
+            self.setlog("Error! Read setting ini file fail! "
                         "please create UTF-16 format " + filename + " in tool path", 'error')
+            return error_Type.FILE_ERROR
 
     def write_config(self, filename, sections, key, value):
         try:
@@ -198,6 +208,7 @@ class replace_Sub_Gui(Frame):
         except Exception as ex:
             self.setlog("Error! Write setting to ini file fail, "
                         "please create UTF-16 format "+filename+" in tool path", 'error')
+            return error_Type.FILE_ERROR
 
     def store_origin_file_to_backup_folder(self, file, back_folder):
         shutil.copy2(file, back_folder)
@@ -235,9 +246,9 @@ class replace_Sub_Gui(Frame):
                     self.store_origin_file_to_backup_folder(i, self.user_input_path+'\\'+backup_folder_name)
                     sub_content_temp_lv = sub_content_lv
                     # -----convert to TC language-----
-                    self.setlog("Convert: " + i, 'info')
+                    self.setlog("Convert: " + i)
                     tw_str_lv = langconver.s2tw(sub_content_lv)
-                    self.setlog("Replace font set: " + i)
+                    self.setlog("Replace font set: " + i, 'info2')
                     tw_str_lv = replace_sub.replace_specif_string(tw_str_lv, subdata_dic)
                     if sub_content_temp_lv != tw_str_lv:
                         subcontent_write_h = open(i, 'w', encoding='utf8')
@@ -251,9 +262,9 @@ class replace_Sub_Gui(Frame):
             # -----for utf8 and utf16 format-----
             sub_content_temp_lv = sub_content_lv
             # -----convert to TC language-----
-            self.setlog("Convert: "+i, 'info')
+            self.setlog("Convert: "+i)
             tw_str_lv = langconver.s2tw(sub_content_lv)
-            self.setlog("Replace font set: "+i)
+            self.setlog("Replace font set: "+i, 'info2')
             tw_str_lv = replace_sub.replace_specif_string(tw_str_lv, subdata_dic)
             # -----if sub file content is changed, write to origin file-----
             if sub_content_temp_lv != tw_str_lv:
@@ -263,6 +274,7 @@ class replace_Sub_Gui(Frame):
         return status_lv
 
     def replace_all_sub_in_path(self):
+        w_file_stat_lv = error_Type.NORMAL
         # -----Clear text widge for log-----
         self.log.config(state="normal")
         self.log.delete('1.0', END)
@@ -282,6 +294,11 @@ class replace_Sub_Gui(Frame):
         # -----get config ini file setting-----
         self.subpath_ini = self.read_config(sub_setting_name, 'Global', 'subpath')
         self.subfiletype_list_ini = self.read_config(sub_setting_name, 'Global', 'subtype')
+        if self.subpath_ini == error_Type.FILE_ERROR or self.subfiletype_list_ini == error_Type.FILE_ERROR:
+            tkinter.messagebox.showerror("Error",
+                                         "Error! Read setting ini file fail! "
+                                         "please create UTF-16 format " + sub_setting_name + " in tool path")
+            return
 
         # -----remove '\' or '/' in end of path string-----
         self.user_input_path = re.sub(r"/$", '', self.user_input_path)
@@ -291,11 +308,17 @@ class replace_Sub_Gui(Frame):
         if not self.user_input_path == self.subpath_ini:
             self.setlog("Write new path setting to: " + sub_setting_name, "info")
             # print("path not match, write new path to ini")
-            self.write_config(sub_setting_name,  'Global', 'subpath', self.user_input_path)
+            w_file_stat_lv = self.write_config(sub_setting_name,  'Global', 'subpath', self.user_input_path)
         if not self.user_input_type == self.subfiletype_list_ini:
             self.setlog("Write new type setting to: " + sub_setting_name, "info")
             # print("type not match, write new type list to ini")
-            self.write_config(sub_setting_name, 'Global', 'subtype', self.user_input_type)
+            w_file_stat_lv = self.write_config(sub_setting_name, 'Global', 'subtype', self.user_input_type)
+
+        if w_file_stat_lv == error_Type.FILE_ERROR:
+            tkinter.messagebox.showerror("Error",
+                                         "Error! Write setting ini file fail! "
+                                         "please create UTF-16 format " + sub_setting_name + " in AJSub path")
+            return
 
         # ----Split file type string and store to list-----
         re_lv = re.sub(r' ', '', self.user_input_type)
@@ -326,7 +349,7 @@ class replace_Sub_Gui(Frame):
             self.setlog("***Success! Convert and Replace all file done.***", "info")
             tkinter.messagebox.showinfo("message", "Convert and Replace all file done.")
         else:
-            self.setlog("***Error! Convert and Replace file error, please check tne log.***", "error")
+            self.setlog("***Error! Convert and Replace file error, please check the log.***", "error")
             tkinter.messagebox.showerror("message Error", "Convert and Replace file error, please check the log.")
 
 
@@ -339,32 +362,37 @@ def check_all_file_status():
         return False
     return True
 
-try:
+
+if __name__ == '__main__':
+    # -----MessageBox will create tkinter, so create correct setting tkinter first
+    root = Tk()
+    root.title(title)
+    root.iconbitmap('icons\\main.ico')
+
+    if not check_all_file_status():
+        tkinter.messagebox.showerror("Error", "Necessary file is not found! \n\nPlease check below is exist or "
+                                              "please re-install AJSub:\n"
+                                              "1. " + sub_setting_name + "\n"
+                                              "2. " + sub_database_name + "\n"
+                                              "3. icons\\main.ico")
+        sys.exit(0)
+
+    try:
+        # -----Get setting from Settings.ini-----
+        file_ini_h = open(sub_setting_name, encoding='utf16')
+        config_h = configparser.ConfigParser()
+        config_h.read_file(file_ini_h)
+        file_ini_h.close()
+        subpath = config_h.get('Global', 'subpath')
+        subfiletype_list = config_h.get('Global', 'subtype')
+    except:
+        tkinter.messagebox.showerror("Error", "Read setting fail " + sub_setting_name + " or " + sub_database_name + " fail!\n"
+                                              "Please check these file is correct (unicode format) or re-install AJSub")
+        sys.exit(0)
+
     # -----Get database list to dic structure-----
     sub_data_dic = replace_sub.get_database_list(sub_database_name)
-    # -----Get setting from Settings.ini-----
-    file_ini_h = open(sub_setting_name, encoding='utf16')
-    config_h = configparser.ConfigParser()
-    config_h.read_file(file_ini_h)
-    file_ini_h.close()
-    subpath = config_h.get('Global', 'subpath')
-    subfiletype_list = config_h.get('Global', 'subtype')
-except:
-    # messagebox will create empty windows if root tkinter isn't create, so create tkinter first
-    root = Tk()
-    root.title(title)
-    root.iconbitmap('icons\\main.ico')
-    app = replace_Sub_Gui(master=root, subfilepath_ini=subpath,
-                          subfiletype_ini=subfiletype_list, help_text=help_text)
-
-    tkinter.messagebox.showerror("Error", "necessary file is not found! Please re-install AJSub")
-    sys.exit(0)
-
-# -----Start GUI-----
-if __name__ == '__main__':
-    root = Tk()
-    root.title(title)
-    root.iconbitmap('icons\\main.ico')
+    # -----Start GUI class-----
     app = replace_Sub_Gui(master=root, subfilepath_ini=subpath,
                           subfiletype_ini=subfiletype_list, help_text=help_text)
     # -----Start main loop-----
